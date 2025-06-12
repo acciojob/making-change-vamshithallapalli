211a3b73ca9ad08c8cd0cc837cb0ac2cec28336e
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-12 12:00:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const makeChange = (c) => {
   // your name here
-};
+	let change = {
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	}
 
+	change.q = Math.floor(amount/25);
+	amount %= 25;
+
+	change.d = Math.floor(amount/10);
+	amount %= 10;
+
+	change.n = Math.floor(amount/5);
+	amount %= 5;
+
+	change.q = amount;
+
+	return change;
+};
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
